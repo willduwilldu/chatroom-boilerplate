be5c5fb90e6dc61d9984693bc8eea01bd8e2c5b0
Will <williamd@Williams-MacBook-Pro.local>
2014-11-20 17:07:48 -0500

    Added code to enable emojis to message box

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -4,33 +4,22 @@ jQuery(function($) {
     
     // Create New Socket Connection using Socket.io
     var socket = io();
-    // var emojis = [
-    //         {typed:'sick',link:'1f621.png'},
-    //         {typed:'>:(',link:'1f621.png'},
-    //         {typed:':/',link:'1f613.png'},
-    //         {typed:';D',link:'1f609.png'},
-    //         {typed:':)',link:'1f604.png'},
-    //         {typed:'=)',link:'1f603.png'},
-    //         {typed:'D:',link:'1f601.png'},
-    //         {typed:'T.T',link:'1f62d.png'},
-    //         {typed:'o.0',link:'1f61c.png'},
-    //     ];
-
-    // function replaceEmoji(){
-    //     for (var i in emojis){
-    //         var obj=emojis[i];
-    //         console.log(obj.link);
-    //         text.replace(new RegExp(text, obj.typed),'<img src="'+obj.link+'">');
-    //     }
-    //     console.log(text);
-    // };
-
+    var emojis = [
+            {typed:'sick',link:'1f621.png'},
+            {typed:'>:(',link:'1f621.png'},
+            {typed:':/',link:'1f613.png'},
+            {typed:';D',link:'1f609.png'},
+            {typed:':)',link:'1f604.png'},
+            {typed:'=)',link:'1f603.png'},
+            {typed:'D:',link:'1f601.png'},
+            {typed:'T.T',link:'1f62d.png'},
+            {typed:'o.0',link:'1f61c.png'},
+        ];
     var text;
 
     //Send a message to the server
     $('.send').on('click',function(){
     	text=$('input').val();
-        // replaceEmoji();
     	socket.emit('message', text); //pass message to server!
         $('input').val('');
         text='';
@@ -39,6 +28,14 @@ jQuery(function($) {
     //listening from server for the update in this socket
     //Recieve Update Event from the server
     socket.on('update', function(msg){
+        
+        // Loop through emojis
+        $.each(emojis, function(){
+            msg = msg.replace(this.typed, '<img class="small-emoji" src="/assets/images/' + this.link + '">');
+            console.log(this.typed, msg);
+        });
+
+        // Append message
     	$('.messages').append(msg);
     });
     $('img').on('click',function(){
